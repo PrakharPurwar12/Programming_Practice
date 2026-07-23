f484331d1ed14077bcd1cd34cb8e694b9eed996a
Prakhar Purwar <149057767+PrakharPurwar12@users.noreply.github.com>
2026-07-23 22:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 198.9 MB (60.63%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 2) {
+            return n;
+        }
+        int msb = 31 - __builtin_clz(n);
+        return 1 << (msb + 1);
+    }
+};
